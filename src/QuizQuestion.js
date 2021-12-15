673efff2f72408d4ba909fee99333f7744445959
essennejaye <sjeffers02@gmail.com>
2021-12-15 14:53:05 -0700

    qq_component

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -5,12 +5,11 @@ class QuizQuestion extends Component {
     return (
       <main>
         <section>
-          <p>{this.props.quiz_question.instruction_text[0]}</p>
+          <p>{this.props.quiz_question.instruction_text}</p>
         </section>
         <section className='buttons'>
           <ul>
             <li>{this.props.quiz_question.answer_options[0]}</li>
-            //quiz question button logic goes here
           </ul>
         </section>
       </main>
