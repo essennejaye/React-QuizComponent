2c293c9936b6121b4a68dae5efd6f4b482f0d092
essennejaye <sjeffers02@gmail.com>
2021-12-15 23:15:32 -0700

    error-message

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,26 +2,39 @@ import React, { Component } from 'react';
 import QuizQuestionButton from './QuizQuestionButton';
 
 class QuizQuestion extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { incorrectAnswer: false };
+  }
   handleClick(buttonText) {
     if (buttonText == this.props.quiz_question.answer) {
-      this.props.showNextQuestionHandler()
+      this.props.showNextQuestionHandler();
+      this.setState({ incorrectAnswer:false });
+    } else {
+      this.setState({ incorrectAnswer:true });
     }
   }
   render() {
     return (
       <main>
+        {this.state.incorrectAnswer ? (
+          <p className='error'>Sorry, that's not right</p>
+        ) : <p className='error'></p>}
         <section>
           <p>{this.props.quiz_question.instruction_text}</p>
         </section>
         <section className='buttons'>
-        <ul>
-          {this.props.quiz_question.answer_options.map((answer_option, index) =>
-            <QuizQuestionButton
-              clickHandler={this.handleClick.bind(this)}
-               key={index}
-               button_text={answer_option} />
-           )}
-            </ul>
+          <ul>
+            {this.props.quiz_question.answer_options.map(
+              (answer_option, index) => (
+                <QuizQuestionButton
+                  clickHandler={this.handleClick.bind(this)}
+                  key={index}
+                  button_text={answer_option}
+                />
+              )
+            )}
+          </ul>
         </section>
       </main>
     );
